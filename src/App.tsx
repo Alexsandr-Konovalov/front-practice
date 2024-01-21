b372cea6b08bc2638c4d3cc02e20632cb5e87de4
Alexandr Konovalov <expert.86@inbox.ru>
2024-01-21 23:07:17 +0400

    added button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  const [clicks, setClicks] = React.useState(0);
+
+  function increaseClicks() {
+    setClicks(clicks + 1);
+    }
+    
   return (
     <div className="App">
       <header className="App-header">
-        
+      <RedButton onClick={increaseClicks}>НЕ НАЖИМАТЬ!</RedButton>
+      <Counter>и всё же ты нажал {clicks} раз</Counter>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const RedButton = styled.button ({
+  height: 200,
+  width: 200,
+  backgroundColor: '#E80000',
+  color: '#FFFFFF',
+  fontSize: 18,
+  fontWeight: 700,
+  borderRadius: '50%',
+  border: '3px solid #FFFFFF',
+  cursor: 'pointer',
+
+  '&:hover': {
+    backgroundColor: '#F80000',
+  },
+
+  '&:active': {
+    backgroundColor: '#D80000',
+  },
+
+});
+
+const Counter = styled.p({
+  fontSize: 24,
+  color: '#FFFFFF',
+  });
