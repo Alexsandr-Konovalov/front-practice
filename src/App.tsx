69512003ec231a04c5a24a3b1340e2e381afebe6
Alexandr Konovalov <expert.86@inbox.ru>
2024-01-22 10:19:48 +0400

    added a function to reset the counter

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,15 @@ function App() {
   function increaseClicks() {
     setClicks(clicks + 1);
     }
-    
+  function resetClicks() {
+    setClicks(0);
+  }
   return (
     <div className="App">
       <header className="App-header">
       <RedButton onClick={increaseClicks}>НЕ НАЖИМАТЬ!</RedButton>
       <Counter>и всё же ты нажал {clicks} раз</Counter>
+      <ResetButton onClick={resetClicks}>Сброс</ResetButton>
       </header>
     </div>
   );
@@ -42,8 +45,28 @@ const RedButton = styled.button ({
   },
 
 });
+const ResetButton = styled.button ({
+  height: 50,
+  width: 300,
+  backgroundColor: '#30d5c8',
+  color: '#000000',
+  fontSize: 16,
+  fontWeight: 700,
+  borderRadius: '10%',
+  border: '3px solid #000',
+  cursor: 'pointer',
+
+  '&:hover': {
+    backgroundColor: '#800080',
+  },
+
+  '&:active': {
+    backgroundColor: '#660066',
+  },
+
+});
 
 const Counter = styled.p({
   fontSize: 24,
   color: '#FFFFFF',
-  });
+});
